d1dae162c9b14fefd98732ce276060f1e0338f53
DanielPhilosoph <danielph9@gmail.com>
2022-01-27 13:15:06 +0200

    cdk final

--- CDK-cymotive-task/lib/cdk-cymotive-task-stack.ts
+++ CDK-cymotive-task/lib/cdk-cymotive-task-stack.ts
@@ -3,7 +3,10 @@ import * as lambda from "@aws-cdk/aws-lambda";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as apigw from "@aws-cdk/aws-apigateway";
 import * as s3 from "@aws-cdk/aws-s3";
-import { S3EventSource } from "@aws-cdk/aws-lambda-event-sources";
+import {
+  S3EventSource,
+  ApiEventSource,
+} from "@aws-cdk/aws-lambda-event-sources";
 import {
   AnyPrincipal,
   Effect,
@@ -68,20 +71,30 @@ export class CdkCymotiveTaskStack extends cdk.Stack {
 
     //? ----------------- policies and permissions ---------------------------------
 
-    // create a policy statement
-    const s3_allow_all_policy = new PolicyStatement({
+    // create a policy statement - //? For s3 bucket
+    const s3_allow_all_policy_s3 = new PolicyStatement({
       effect: Effect.ALLOW,
       principals: [new AnyPrincipal()],
       actions: ["s3:*"],
-      resources: [`${table.tableArn}/*`],
+      resources: [`${bucket.bucketArn}/*`, `${bucket.bucketArn}`],
+    });
+
+    // create a policy statement - //? For lambda function
+    const s3_allow_all_policy = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["s3:*"],
+      resources: [`${bucket.bucketArn}/*`, `${bucket.bucketArn}`],
     });
 
     // create a policy statement
     const dynamodb_allow_all_policy = new PolicyStatement({
       effect: Effect.ALLOW,
-      principals: [new AnyPrincipal()],
       actions: ["dynamodb:*"],
-      resources: [`${table.tableArn}/*`],
+      resources: [
+        `${table.tableArn}`,
+        `${table.tableArn}/*`,
+        "arn:aws:dynamodb:eu-west-3:806333949423:table/*",
+      ],
     });
 
     // Create policies to s3
@@ -89,7 +102,7 @@ export class CdkCymotiveTaskStack extends cdk.Stack {
       bucket: bucket,
     });
 
-    s3BucketPolicyS3.document.addStatements(s3_allow_all_policy);
+    s3BucketPolicyS3.document.addStatements(s3_allow_all_policy_s3);
 
     // Grant lambda all dynamodb permissions
     ingestLambda.role?.attachInlinePolicy(
@@ -98,9 +111,62 @@ export class CdkCymotiveTaskStack extends cdk.Stack {
       })
     );
 
+    analyzerLambda.role?.attachInlinePolicy(
+      new Policy(this, " s3_dynamodb_allow_all", {
+        statements: [dynamodb_allow_all_policy, s3_allow_all_policy],
+      })
+    );
+
     // Grant permissions
     table.grantReadWriteData(porterLambda);
     bucket.grantPut(porterLambda);
+
+    //? ----------------- API and routes ---------------------------------
+    // create the API Gateway with one method and path
+    const api = new apigw.RestApi(this, "idsgateway-cdk", {
+      deployOptions: {
+        stageName: "api",
+      },
+      defaultCorsPreflightOptions: {
+        /**
+         * The allow rules are a bit relaxed.
+         * I would strongly advise you to narrow them down in your applications.
+         */
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: ["*"],
+        allowCredentials: true,
+      },
+    });
+
+    //? POST /
+    api.root.addMethod("POST", new apigw.LambdaIntegration(porterLambda));
+
+    //? GET /numberofanomalies
+    const numberOfAnomalies = api.root.addResource("numberofanomalies");
+    numberOfAnomalies.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(analyzerLambda)
+    );
+
+    //? GET /numberofreports
+    const numberOfReports = api.root.addResource("numberofreports");
+    numberOfReports.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(analyzerLambda)
+    );
+
+    //? GET /numberofvehicles
+    const numberOfVehicles = api.root.addResource("numberofvehicles");
+    numberOfVehicles.addMethod(
+      "GET",
+      new apigw.LambdaIntegration(analyzerLambda)
+    );
+
+    //? Log the url to API
+    new cdk.CfnOutput(this, "HTTP API URL", {
+      value: api.url ?? "Something went wrong with the deploy",
+    });
 
     //? -------------------- Triggers ------------------------------------
 
@@ -110,47 +176,5 @@ export class CdkCymotiveTaskStack extends cdk.Stack {
     });
 
     ingestLambda.addEventSource(s3PutEventSource);
-
-    //? ----------------- API and routes ---------------------------------
-    // create the API Gateway with one method and path
-    const api = new apigw.RestApi(this, "idsgateway-cdk", {
-      deployOptions: {
-        stageName: "api",
-      },
-      defaultCorsPreflightOptions: {
-        allowHeaders: [
-          "Content-Type",
-          "X-Amz-Date",
-          "Authorization",
-          "X-Api-Key",
-        ],
-        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowCredentials: true,
-        allowOrigins: ["http://localhost:3000"],
-      },
-    });
-
-    api.root.addMethod("POST", new apigw.LambdaIntegration(porterLambda));
-    const numberOfAnomalies = api.root.addResource("numberofanomalies");
-    numberOfAnomalies.addMethod(
-      "GET",
-      new apigw.LambdaIntegration(analyzerLambda)
-    );
-
-    const numberOfReports = api.root.addResource("numberofreports");
-    numberOfReports.addMethod(
-      "GET",
-      new apigw.LambdaIntegration(analyzerLambda)
-    );
-
-    const numberOfVehicles = api.root.addResource("numberofvehicles");
-    numberOfVehicles.addMethod(
-      "GET",
-      new apigw.LambdaIntegration(analyzerLambda)
-    );
-
-    new cdk.CfnOutput(this, "HTTP API URL", {
-      value: api.url ?? "Something went wrong with the deploy",
-    });
   }
 }
